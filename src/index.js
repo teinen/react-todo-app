4cffc38ea221cc12408744d823e1960f582db19f
諦念 <teinen.github@gmail.com>
2018-04-18 06:55:17 +0900

    Add bootstrap and each components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+// ToDo App Component
+class ToDoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      createToDoArea: "Add New ToDo",
+      displayToDoListArea: "ToDo List",
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <div className="todo-app">
+          <h2>{this.state.createToDoArea}</h2>
+          <ToDoCreator />
+
+          <h2>{this.state.displayToDoListArea}</h2>
+          <ToDoList />
+        </div>
+      </div>
+    );
+  }
+}
+
+// ToDo Creator Component
+class ToDoCreator extends React.Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+// ToDo List Component
+class ToDoList extends React.Component {
+  render() {
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+// ==============================================
+ReactDOM.render(
+  <ToDoApp />,
+  document.getElementById('root')
+);
