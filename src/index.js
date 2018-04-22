43d438ec1d78c8ed4ad444288d74035ada54d2a6
諦念 <teinen.github@gmail.com>
2018-04-23 07:17:15 +0900

    Implement ToDo Check function

--- src/index.js
+++ src/index.js
@@ -7,17 +7,48 @@ class ToDoApp extends React.Component {
   constructor(props) {
     super(props);
 
-    this.bindFunc = this.addToDo.bind(this);
+    this.bindFunc1 = this.addToDo.bind(this);
+    this.bindFunc2 = this.changeStatus.bind(this);
 
     this.state = {
-      todos: [],
+      todos: [
+        {
+          itemName: "sample ToDo 1",
+          status: false,
+        },
+        {
+          itemName: "sample ToDo 2",
+          status: false,
+        },
+      ],
     }
   }
 
-  // Callback func
+  // Callback function
+  // Add new ToDo
   addToDo(newToDo) {
     this.setState({
       todos: this.state.todos.concat(newToDo),
+    });
+  }
+
+  // Change ToDo Status (true <=> false)
+  changeStatus(i) {
+    const todos = this.state.todos.slice();
+    todos[i].status = !todos[i].status;
+
+    var checkButton = document.getElementById(i);
+
+    if(todos[i].status === true) {
+      checkButton.classList.remove("btn-success");
+      checkButton.classList.add("btn-secondary");
+    } else {
+      checkButton.classList.remove("btn-secondary");
+      checkButton.classList.add("btn-success");
+    }
+
+    this.setState({
+      todos: todos,
     });
   }
 
@@ -26,10 +57,11 @@ class ToDoApp extends React.Component {
       <div>
         <div className="todo-app">
           <ToDoCreator
-            addToDo={this.bindFunc}
+            addToDo={this.bindFunc1}
           />
           <ToDoList
             todos={this.state.todos}
+            onClick={this.bindFunc2}
           />
         </div>
       </div>
@@ -57,10 +89,9 @@ class ToDoCreator extends React.Component {
     this.props.addToDo(
       {
         itemName: this.state.text,
-        isDone: true,
+        status: false,
       }
     );
-
     this.setState({
       text: "",
     });
@@ -71,9 +102,13 @@ class ToDoCreator extends React.Component {
       <div className="container createToDoArea">
         <div className="jumbotron">
           <h2>{this.state.areaTitle}</h2>
-          <div className="input-group mb-3">
-            <input type="text" className="form-control" value={this.state.text} onChange={e => {this.onChange(e)}} />
-            <button type="button" className="btn btn-primary btn-lg btn-block createButton"
+          <div className="input-group input-group-lg">
+            <div className="input-group-prepend">
+              <span className="input-group-text bg-secondary text-white">ToDo title</span>
+            </div>
+            <input type="text" id="inputToDoField" className="form-control" value={this.state.text}
+              onChange={e => {this.onChange(e)}} />
+            <button type="button" className="btn btn-primary btn-lg btn-block create-button"
               onClick={() => {this.onClick()}}>Create ToDo</button>
           </div>
         </div>
@@ -91,32 +126,43 @@ class ToDoList extends React.Component {
     }
   }
 
-  createToDoList(todos) {
-    console.log(todos);
+  renderItem(todo) {
+    if(todo.status == true) {
+      return (
+        <span className="todo-item finished">
+          <del>{todo.itemName}</del>
+        </span>
+      );
+    } else {
+      return (
+        <span className="todo-item">
+          {todo.itemName}
+        </span>
+      );
+    }
+  }
 
+  createToDoList(todos) {
     const todoList = todos.map((todo,i) => {
         return (
-          <li key={i} className="list-group-item">
-            <div class="input-group mb-3">
-              <div class="input-group-prepend">
-                <div class="input-group-text">
-                  <input type="checkbox" aria-label="Checkbox for following text input" checked/>
-                </div>
-              </div>
-              <input type="text" class="form-control" aria-label="Text input with checkbox" />
-            </div>
-            {todo.itemName}
+          <li key={i} className="list-group-item align-middle">
+            <button type="button" className="btn btn-success todo-check-button"
+              id={i} onClick={() => {this.props.onClick(i)}}>DONE</button>
+            {this.renderItem(todo)}
+            <button type="button" className="btn btn-danger todo-delete-button">DEL</button>
           </li>
         );
       }
     );
 
     return(
-      <div className="container">
+      <div>
         <h2>{this.state.areaTitle}</h2>
-        <ul className="list-group">
-          {todoList}
-        </ul>
+        <div className="container todo-list-content">
+          <ul className="list-group">
+            {todoList}
+          </ul>
+        </div>
       </div>
     );
   }
